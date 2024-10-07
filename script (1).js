9ea2d09a1a3dd5ed2f4e98fd7a8c367b01e7fbe5
Rubal489 <rubal.21scse1120009@galgotiasuniversity.edu.in>
2024-10-07 16:35:21 +0530

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,8 @@
+// Add event listeners for animations
+document.addEventListener("DOMContentLoaded", function() {
+    AOS.init({
+        duration: 1000,
+        easing: "ease-in-out",
+        once: true,
+    });
+});
